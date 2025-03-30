3288a67a30e3295692de35501b3268613b8ac00e
alshapton <alshapton@gmail.com>
2025-03-30 23:10:56 +0100

    Added TYPE column to collection table

--- collection.md
+++ collection.md
@@ -1,10 +1,10 @@
 # Collection of Motorola MC6800 artefacts as at 30-03-2025
 
 
-| File | Description | 
-|----- |------------ |
-| [MEX6816-22D](/Hardware/EXORciser/@MEX6816-22D.md)|16K Dynamic RAM|
-| [MC68488 Card](/images/MC68488.1.png)|MC68488 GPIA for IEEE-488 Bus|
-| [M68MM07](/images/M68MM07.1.png)|M68MM07 Quad Communications Micromodule)|
-| [MC6850 (1 Mhz) <br />MC68A50 (1.5 Mhz) <br />MC68B50 (2 Mhz)](/images/MC6850.1.png)|Asynchronous Communications Interface Adapter|
-| [MPU Vocabulary](/images/MPU_Vocabulary.1.png)|MPU Vocabulary (First Edition 1977)|
+| File | Description | Type |
+|----- |------------ |--    |
+| [MEX6816-22D](/Hardware/EXORciser/@MEX6816-22D.md)|16K Dynamic RAM|Hardware|
+| [MC68488 Card](/images/MC68488.1.png)|MC68488 GPIA for IEEE-488 Bus|Reference Card|
+| [M68MM07](/images/M68MM07.1.png)|M68MM07 Quad Communications Micromodule)|Datasheet|
+| [MC6850 (1 Mhz) <br />MC68A50 (1.5 Mhz) <br />MC68B50 (2 Mhz)](/images/MC6850.1.png)|Asynchronous Communications Interface Adapter|Datasheet|
+| [MPU Vocabulary](/images/MPU_Vocabulary.1.png)|MPU Vocabulary (First Edition 1977)|Generic Document|
--- collection.py
+++ collection.py
@@ -4,6 +4,7 @@
 
 import glob
 import time
+import os
 
 # Find all .txt files in the current directory and subdirectories
 files = glob.glob('**/*.md', recursive=True)
@@ -11,18 +12,34 @@ with open("collection.md","w") as c:
     c.write ('# Collection of Motorola MC6800 artefacts as at '+ time.strftime("%d-%m-%Y") + '\n')
 
     c.write('\n\n')
-    c.write('| File | Description | \n') 
-    c.write('|----- |------------ |\n')
+    c.write('| File | Description | Type |\n') 
+    c.write('|----- |------------ |--    |\n')
 
     for file in files:
         if (file != "README.md" and 
             file != "collection.md" and
             "@" not in file):
+            
             with open(file) as f:
+                type = os.path.dirname(file)
+                match type:
+                    case "Documents/Datasheets":
+                        doc_type = "Datasheet"
+                    case "Documents/ReferenceCards":
+                        doc_type = "Reference Card" 
+                    case "Documents/Generic":
+                        doc_type = "Generic Document"
+                    case "software":
+                        doc_type = "Software"
+                    case "Hardware/EXORciser":
+                        doc_type = "Hardware"
+                    case _:
+                        doc_type = " "
+                doc_type = doc_type + "|"
                 for line in f:
                     if ':white_check_mark:' in line:
                         splitline = line.split('|')
                         part_number = splitline[1].strip().replace(':white_check_mark:','')
                         description = splitline[2].strip()
-                        c.write('|' + part_number + '|' + description + '|\n')
+                        c.write('|' + part_number + '|' + description + '|' + doc_type + '\n')
                             
