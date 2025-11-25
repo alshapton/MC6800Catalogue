d95ec7c79b951d2968e602383ac55d74f24c11ef
alshapton <alshapton@gmail.com>
2025-11-25 16:54:04 +0000

    Updates:
    - Aligned MC6880A
    - Aligned MC6882
    - Aligned MC6883
    - Aligned MC6885
    - Aligned MC6886

--- xbuild.py
+++ xbuild.py
@@ -2417,7 +2417,7 @@ while True:
             console.print(output, style="info") 
             
         case "9":
-            output=read_db("SELECT * FROM ics WHERE parent = 'MC6875' order by icid asc;")
+            output=read_db("SELECT * FROM ics WHERE parent = 'MC6880A' order by icid asc;")
             for chipinfo in output:
                 filename=chipinfo["filename"]
                 write_IC(filename,chipinfo)
